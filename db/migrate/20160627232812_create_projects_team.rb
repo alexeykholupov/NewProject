230e85e707c513cda23a6821474177263f2beb9f
Alexey Kholupov <asterminate@gmail.com>
2016-06-28 12:58:21 +0000

    Adding Navigation pannel, adding table view to index pages in Teams,Projects,Posts,Tasks

--- db/migrate/20160627232812_create_projects_team.rb
+++ db/migrate/20160627232812_create_projects_team.rb
@@ -4,5 +4,7 @@ class CreateProjectsTeam < ActiveRecord::Migration
       t.references :project, index: true, foreign_key: true
       t.references :team, index: true, foreign_key: true
     end
+      add_index :projects_teams, :project_id
+      add_index :projects_teams, :team_id
   end
 end
